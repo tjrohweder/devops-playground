f1c5127981abceb43485f779b598a1dd00c4a07a
tjrohweder <tjrohweder@gmail.com>
2025-07-15 23:34:09 -0300

    Adding basic structure for terraform scripts

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "aws_azs" {
+  type = list()
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
