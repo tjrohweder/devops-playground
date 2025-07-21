943ceed4b6ecbfa23e54d88ad06f295b13d17a1d
tjrohweder <tjrohweder@gmail.com>
2025-07-20 23:57:59 -0300

    Fixing variable type

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,9 +4,5 @@ variable "env" {}
 #VPC
 variable "vpc_name" {}
 variable "vpc_cidr" {}
-variable "private_subnets" {
-  type = list()
-}
-variable "public_subnets" {
-  type = list()
-}
+variable "private_subnets" {}
+variable "public_subnets" {}
