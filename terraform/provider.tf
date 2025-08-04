ced64799c368507e592b7804072d0d5768b2f8b7
tjrohweder <tjrohweder@gmail.com>
2025-08-04 17:23:50 -0300

    Adding tfstate backend configuration

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket = "tjrohweder-terraform-tfstate"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
